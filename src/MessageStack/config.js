66c724d893e3aa12c94fb4b0a190a3dcbbc5bf7d
nai golf <niy_golf@hotmail.com>
2019-02-12 00:26:38 +0700

    Update config.js

--- src/MessageStack/config.js
+++ src/MessageStack/config.js
@@ -4,6 +4,6 @@ export default {
     host: 'm15.cloudmqtt.com',
     username: 'czpxynah',
     password: '5wdKnv7HUR1U',
-    clientId: 'ESP32Client' + Math.random().toString(16).substr(2, 8),
+    clientId: 'bledemo_' + Math.random().toString(16).substr(2, 8),
     channel: '/beacons/office'
 };
